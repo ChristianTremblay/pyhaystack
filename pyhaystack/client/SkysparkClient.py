819af6c27d849e69733637eac28fc69ad713ae9c
PilatOfIsrael <igor.zhalkin@gmail.com>
2015-10-27 18:05:23 +0200

    Brought to work

--- pyhaystack/client/SkysparkClient.py
+++ pyhaystack/client/SkysparkClient.py
@@ -14,44 +14,50 @@ class Connect(hc.Connect):
     This class connects to skyspark and fetch haystack servlet
     A session is open and authentication will persist
     """
+
     def __init__(self, url, username, password, proj, **kwargs):
         """
         Define skyspark specific local variables : url
         Calls the authenticate function
         """
+
         proj = str(proj)
         hc.Connect.__init__(self, url, username, password, proj, **kwargs)
-        self.loginURL = self.baseURL + "/auth/"+proj+"/api?"+username
-        self.saltURL = self.baseURL + "/auth/"+proj+"/salt"
-        self.queryURL = self.baseURL + "/api/"+proj+"/"
-        self.requestAbout = "/api/"+proj+"/about"
+        self.loginURL = self.baseURL + "/auth/" + proj + "/api?" + username
+        self.saltURL = self.baseURL + "/auth/" + proj + "/salt"
+        self.queryURL = self.baseURL + "/api/" + proj + "/"
+        self.requestAbout = "about"
         self.password = password
+        # self.about = self.read(self.requestAbout)
         self.authenticate()
 
     def authenticate(self):
-        print('pyhaystack %s | Authentication to %s' % (info.__version__,self.loginURL))
-        r = requests.get(self.loginURL)
+        print('pyhaystack %s | Authentication to %s' % (info.__version__, self.loginURL))
+        # s = requests.Session()
+        r = self.s.get(self.loginURL)
         response_dict = {}
 
         for line in r.iter_lines():
             key, value = line.split(':')
             response_dict[key] = value
-        HMAC= hmac.new(key=self.password, msg=response_dict['username']+":"+response_dict['userSalt'],
-                     digestmod=hashlib.sha1)
+        HMAC = hmac.new(key=self.password, msg=response_dict['username'] + ":" + response_dict['userSalt'],
+                        digestmod=hashlib.sha1)
 
         h = base64.b64encode(HMAC.digest())
         digest_hash = hashlib.sha1()
-        digest_hash.update(h+":"+response_dict['nonce'])
+        digest_hash.update(h + ":" + response_dict['nonce'])
         digest = digest_hash.digest().encode("base64").rstrip('\n')
 
         head = {"Content-Type": 'text/plain',
-                "Host" : self.baseURL,
+                "Host": self.baseURL,
                 "User-Agent": 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
                 "charset": 'utf-8'
                 }
-        data = {"nonce": response_dict['nonce'],
-                "digest": digest,
-                }
+        # data = {"nonce": response_dict['nonce'],
+        #          "digest": digest,
+        #          }
+        data = "nonce:" + response_dict['nonce'] + "\ndigest:" + digest
+        # print data.keys()
         r = requests.Request(method='POST', url=self.loginURL, headers=head, data=data)
         prepared = r.prepare()
 
@@ -70,8 +76,14 @@ class Connect(hc.Connect):
                 '\n'.join('{}:{}'.format(k, v) for k, v in req.headers.items()),
                 req.body,
             ))
-        #print data
-        print pretty_print_POST(prepared)
+
+        # print data
+        #print pretty_print_POST(prepared)
+        r = self.s.send(prepared)
+        if str(r.text).find("cookie") != -1:
+            self.isConnected = True
+            self.s.cookies.set(str(r.text).split(":")[1].split("=")[0], str(r.text).split(":")[1].split("=")[1])
+        # print r.text
         # """
         # Login to the server
         # Get the cookie from the server, configure headers, make a POST request with credential informations.
@@ -116,12 +128,15 @@ class Connect(hc.Connect):
         #     print('Connection failed, check your parameters or VPN connection...')
         #
         # #Continue with haystack login
-        # if self.isConnected:
-        #     self.about = self.read(self.requestAbout)
-        #     self.serverName = self.about['rows'][0]['serverName']
-        #     self.haystackVersion = self.about['rows'][0]['haystackVersion']
-        #     self.axVersion = self.about['rows'][0]['productVersion']
-        #     self.timezone = 'America/' + self.read('read?filter=site')['rows'][0]['tz']
-        #     print('Time Zone used : %s' % self.timezone)
-        #     print('Connection succeed with haystack on %s (%s) running haystack version %s' %(self.serverName,self.axVersion,self.haystackVersion))
-        #     self.refreshHisList()
+        if self.isConnected:
+            self.about = self.read(self.requestAbout)
+            self.serverName = self.about['rows'][0]['serverName']
+            self.haystackVersion = self.about['rows'][0]['haystackVersion']
+            self.axVersion = self.about['rows'][0]['productVersion']
+            self.timezone =  self.read('read?filter=site')['rows'][0]['tz']
+            print('Time Zone used : %s' % self.timezone)
+            print('Connection succeed with haystack on %s (%s) running haystack version %s' % (
+                self.serverName, self.axVersion, self.haystackVersion))
+            print self.readAll('(his and temp)')
+
+            # self.refreshHisList()
