5cc4caa54edee9be577dc3aa285ff981cf0da75e
Stuart Longland <stuartl@vrt.com.au>
2016-01-12 08:42:01 +1000

    client.HaystackClient: Fix post

--- pyhaystack/client/HaystackClient.py
+++ pyhaystack/client/HaystackClient.py
@@ -144,16 +144,15 @@ class Connect():
         Helper for POST request
         """
         if headers is None:
-            headers = {'token': ''}
+            headers = {}
 
         headers.update({
             'Accept': 'text/plain; charset=utf-8',
             'Content-Type': 'text/plain; charset=utf-8',
         })
 
-        url = self.queryURL + urlToGet
-        kwargs = self._get_kwargs(headers=headers, **kwargs)
         url = self.queryURL + url
+        kwargs = self._get_kwargs(headers=headers, **kwargs)
         self._log.getChild('http').debug(
                 'Submitting POST request for %s, headers: %s, data: %r',
                 url, kwargs.get('headers',{}), kwargs.get('data',None))
