e752ec2c661007b6ac1d9a6add414c23f5c5f4ba
Stuart Longland <stuartl@vrt.com.au>
2016-04-08 09:25:14 +1000

    util.asyncexc: Implement AsynchronousException.
    This is a helper class to capture exceptions that happen in an
    asynchronous function and bundle them up so they can be handled by the
    callback function.

---
+++ pyhaystack/util/asyncexc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Asynchronous Exception Handler.  This implements a small lightweight object for
+capturing an exception in a manner that can be passed in callback arguments then
+re-raised elsewhere for handling in the callback function.
+
+Typical usage:
+
+    def _some_async_function(…, callback_fn):
+
+        try:
+            do some async op that may fail
+
+            result = ...
+        except: # Capture all exceptions
+            result = AsynchronousException()
+
+        callback_fn(result)
+"""
+
+from sys import exc_info
+from six import reraise
+
+class AsynchronousException(object):
+    def __init__(self):
+        self._exc_info = exc_info()
+
+    def reraise(self):
+        reraise(*self._exc_info)
