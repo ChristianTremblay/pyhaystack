aae6ef88535f5779097ea31d86fda2a20eae5565
Stuart Longland <stuartl@vrt.com.au>
2016-04-12 12:17:35 +1000

    client.widesky: Add VRT WideSky support.

---
+++ pyhaystack/client/ops/vendor/widesky.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+VRT WideSky operation implementations.
+"""
+
+import hszinc
+import fysom
+import json
+import base64
+
+from ....util import state
+from ....exception import HaystackError
+from ....util.asyncexc import AsynchronousException
+
+class WideskyAuthenticateOperation(state.HaystackOperation):
+    """
+    An implementation of the log-in procedure for WideSky.  WideSky uses
+    a M2M variant of OAuth2.0 to authenticate users.
+    """
+
+    def __init__(self, session, retries=2):
+        """
+        Attempt to log in to the VRT WideSky server.  The procedure is as
+        follows:
+
+            POST to auth_dir URI:
+                Headers:
+                    Accept: application/json
+                    Authorization: Basic [BASE64:"[ID]:[SECRET]"]
+                    Content-Type: application/json
+                Body: {
+                    username: "[USER]", password: "[PASSWORD]",
+                    grant_type: "password"
+                }
+            EXPECT reply:
+                Headers:
+                    Content-Type: application/json
+                Body:
+                    {
+                        access_token: ..., refresh_token: ...,
+                        expires_in: ..., token_type: ...
+                    }
+
+        :param session: Haystack HTTP session object.
+        :param retries: Number of retries permitted in case of failure.
+        """
+
+        super(WideskyAuthenticateOperation, self).__init__()
+        self._auth_headers = {
+                'Authorization': 'Basic %s' % base64.b64encode(
+                    ':'.join([session._client_id, session._client_secret])),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+        }
+        self._auth_body = json.dumps({
+            'username': session._username,
+            'password': session._password,
+            'grant_type': 'password',
+        }
+        self._session = session
+        self._auth_result = None
+
+        self._state_machine = fysom.Fysom(
+                initial='init', final='done',
+                events=[
+                    # Event             Current State       New State
+                    ('do_login',        'init',             'login'),
+                    ('login_done',      'login',            'done'),
+                    ('exception',       '*',                'failed'),
+                    ('retry',           'failed',           'login'),
+                    ('abort',           'failed',           'done'),
+                ], callbacks={
+                    'onenterlogin':         self._do_login,
+                    'onenterfailed':        self._do_fail_retry,
+                    'onenterdone':          self._do_done,
+                })
+
+    def go(self):
+        """
+        Start the request.
+        """
+        # Are we logged in?
+        try:
+            self._state_machine.do_login()
+        except: # Catch all exceptions to pass to caller.
+            self._state_machine.exception(result=AsynchronousException())
+
+    def _do_login(self, event):
+        try:
+            self._session._post(self._session._auth_dir,
+                self._on_login, body=self._auth_body,
+                    headers=self._auth_headers, exclude_headers=True)
+        except: # Catch all exceptions to pass to caller.
+            self._state_machine.exception(result=AsynchronousException())
+
+    def _on_login(self, response):
+        """
+        See if the login succeeded.
+        """
+        try:
+            if isinstance(response, AsynchronousException):
+                response.reraise
+
+            content_type = response.headers.get('Content-Type')
+            if content_type is None:
+                raise ValueError('No content-type given in reply')
+
+            if ';' in content_type:
+                content_type = content_type.split(';',1)[0].strip()
+
+            if content_type != 'application/json':
+                raise ValueError('Invalid content type received: %s' % \
+                        content_type)
+
+            # Decode JSON reply
+            reply = json.loads(response.body)
+            for key in ('token_type', 'access_token', 'expires_in'):
+                if key not in reply:
+                    raise ValueError('Missing %s in reply :%s' % (key, reply))
+
+            self._state_machine.login_done(result=reply)
+        except: # Catch all exceptions to pass to caller.
+            self._state_machine.exception(result=AsynchronousException())
+
+    def _do_fail_retry(self, event):
+        """
+        Determine whether we retry or fail outright.
+        """
+        if self._retries > 0:
+            self._retries -= 1
+            self._state_machine.retry()
+        else:
+            self._state_machine.abort(result=event.result)
+
+    def _do_done(self, event):
+        """
+        Return the result from the state machine.
+        """
+        self._done(event.result)
---
+++ pyhaystack/client/widesky.py
@@ -0,0 +1,74 @@
+#!python
+# -*- coding: utf-8 -*-
+"""
+VRT Widesky Client support
+"""
+
+from time import time
+from .session import HaystackSession
+from .ops.vendor.widesky import WideskyAuthenticateOperation
+
+class WideskyHaystackSession(HaystackSession):
+    """
+    The WideskyHaystackSession class implements some base support for
+    NiagaraAX and Niagara4 servers.  This is mainly a convenience for
+    collecting the username and password details.
+    """
+
+    _AUTH_OPERATION = WideskyAuthenticateOperation
+
+    def __init__(self, uri, username, password,
+            client_id, client_secret,
+            api_dir='api', auth_dir='oauth2/token', **kwargs):
+        """
+        Initialise a VRT Widesky Project Haystack session handler.
+
+        :param uri: Base URI for the Haystack installation.
+        :param username: Authentication user name.
+        :param password: Authentication password.
+        :param client_id: Authentication client ID.
+        :param client_secret: Authentication client secret.
+        """
+        super(WideskyHaystackSesNagarasion, self).__init__(
+                uri, api_dir, **kwargs)
+        self._auth_dir = auth_dir
+        self._username = username
+        self._password = password
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._auth_result = None
+
+    @property
+    def is_logged_in(self):
+        """
+        Return true if the user is logged in.
+        """
+        if self._auth_result is None:
+            return False
+        # Return true if our token expires in the future.
+        return (self._auth_result.get('expires_in') or 0.0) > (1000.0 * time())
+
+    # Private methods/properties
+
+    def _on_authenticate_done(self, operation, **kwargs):
+        """
+        Process the result of an authentication operation.  This needs to be
+        implemented in the subclass and should, at minimum, set a flag in the
+        subclass to indicate the authentication state and clear the _auth_op
+        attribute on the base class.
+        """
+        try:
+            self._auth_result = operation.result
+            self._client.headers = {
+                    'Authorization': '%s %s' % (
+                        self._auth_result['token_type'],
+                        self._auth_result['access_token'],
+                    )
+            }
+            self._client.cookies = cookies
+        except:
+            self._auth_result = None
+            self._client.auth = None
+            self._client.cookies = None
+        finally:
+            self._auth_op = None
