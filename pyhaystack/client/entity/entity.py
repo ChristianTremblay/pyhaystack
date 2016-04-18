72f295efcb462c25a5ebd9e095217fc07949d482
Stuart Longland <stuartl@vrt.com.au>
2016-04-18 12:49:17 +1000

    client.entity: Re-work entity interface.
    For two reasons:
    1. I didn't look at my own design that called for tags under a `tags`
    attribute.
    2. It highlighted why arbitrary properties on a Python object can get so
    messy, particularly when `__setattr__` is involved.

--- pyhaystack/client/entity/entity.py
+++ pyhaystack/client/entity/entity.py
@@ -5,7 +5,7 @@
 High-level Entity interface.
 """
 
-import hszinc
+from .tags import ReadOnlyEntityTags, MutableEntityTags
 
 class Entity(object):
     """
@@ -26,35 +26,34 @@ class Entity(object):
         :param entity_id: The entity's fully qualified ID.
         """
 
-        attrs = {
-            '_session': session,
-            '_entity_id': entity_id,
-            '_tags': {},
-            # Does the session support updates?
-            '_mutable': hasattr(session, 'update') \
-                and hasattr(session, 'delete')
-        }
+        if hasattr(session, 'update'):
+            tags = MutableEntityTags(session, entity_id)
+        else:
+            tags = ReadOnlyEntityTags(session, entity_id)
+        self._tags = tags
+        self._valid = True
 
-        if attrs['_mutable']:
-            attrs.update({
-                '_tag_updates': {},
-                '_tag_deletions': set(),
-                '_update_pending': False,
-            })
-        self.__dict__.update(attrs)
+    @property
+    def id(self):
+        """
+        Return the fully qualified ID of this entity.
+        """
+        return self._tags.id
+
+    @property
+    def tags(self):
+        """
+        Return the tags of this entity.
+        """
+        return self._tags
 
     def _update_tags(self, tags):
         """
         Update the value of given tags.
         """
-        for tag, value in tags.items():
-            if value is hszinc.REMOVE:
-                self._tags.pop(tag, None)
-            else:
-                self._tags[tag] = value
-
-        if hasattr(self._session, '_check_entity_type') \
-                and (not self._session._check_entity_type(self)):
+        self._tags._update_tags(tags)
+        if hasattr(self._tags._session, '_check_entity_type') \
+                and (not self._tags._session._check_entity_type(self)):
             self._invalidate()
 
     def _invalidate(self):
@@ -64,33 +63,14 @@ class Entity(object):
         """
         if self._session._entities.get(self._entity_id) is self:
             self._session._entities.pop(self._entity_id, None)
-        self.__dict__['_valid'] = False
+        self._valid = False
 
-    @property
-    def id(self):
-        """
-        Return the fully qualified ID of this entity.
-        """
-        return hszinc.Ref(self._entity_id)
 
-    @property
-    def is_dirty(self):
-        """
-        Returns true if there are modifications pending submission.
-        """
-        if not self._mutable:
-            return False
-        return bool(self._tag_updates) or bool(self._tag_deletions)
-
-    def commit(self, callback=None):
-        """
-        Commit any to-be-sent updates for this entity.
-        """
-        if not self._valid:
-            raise StaleEntityInstanceError()
-        if not self._mutable:
-            raise NotImplementedError('Server does not support updates')
-        raise NotImplementedError('TODO: implement CRUD ops')
+class DeletableEntity(Entity):
+    """
+    Class to represent entities that can be deleted from the Haystack server
+    (the server implements the 'delete' operation).
+    """
 
     def delete(self, callback=None):
         """
@@ -98,102 +78,7 @@ class Entity(object):
         """
         if not self._valid:
             raise StaleEntityInstanceError()
-        if not self._mutable:
-            raise NotImplementedError('Server does not support updates')
         raise NotImplementedError('TODO: implement CRUD ops')
-
-    def revert(self, tags=None):
-        """
-        Revert the named attribute changes, or all changes.
-        """
-        if not self._valid:
-            raise StaleEntityInstanceError()
-        if not self._mutable:
-            # Nothing to do
-            return
-
-        if tags is None:
-            self._tag_updates = {}
-            self._tag_deletions = set()
-        else:
-            for tag in tags:
-                self._tag_updates.pop(tag, None)
-                self._tag_deletions.discard(tag)
-
-    def __getattr__(self, attr):
-        """
-        Retrieve the value of an attribute or tag.
-        """
-        try:
-            return self.__dict__[attr]
-        except KeyError:
-            pass
-
-        if not self._valid:
-            raise StaleEntityInstanceError()
-
-        # Handling of naming collisions, we shall allow tag names to be
-        # prefixed with the string "tag_".  The "tag_" prefix will be stripped.
-        if attr.startswith('tag_'):
-            attr = attr[4:]
-
-        tags = self.__dict__['_tags']
-        if self._mutable:
-            tag_updates = self.__dict__['_tag_updates']
-            tag_deletions = self.__dict__['_tag_deletions']
-
-            if attr in tag_deletions:
-                # This is marked for deletion
-                raise AttributeError
-
-            try:
-                return tag_updates[attr]
-            except KeyError:
-                pass
-
-        try:
-            return tags[attr]
-        except KeyError:
-            raise AttributeError
-
-    def __setattr__(self, attr, value):
-        """
-        Update the value of a tag.
-        """
-        if attr.startswith('tag_'):
-            tag = attr[4:]
-        else:
-            tag = attr
-
-        if not self._valid:
-            raise StaleEntityInstanceError()
-
-        if self._mutable and (tag in self._tags):
-            if value is not hszinc.REMOVE:
-                self._tag_updates[tag] = value
-                self._tag_deletions.discard(tag)
-            else:
-                self._tag_updates.pop(tag, None)
-                self._tag_deletions.add(tag)
-            return
-
-        super(Entity, self).__setattr__(attr, value)
-
-    def __delattr__(self, attr):
-        """
-        Delete a tag.
-        """
-        if attr.startswith('tag_'):
-            tag = attr[4:]
-        else:
-            tag = attr
-
-        if self._mutable and (tag in self._tags):
-            self._tag_updates.pop(tag, None)
-            self._tag_deletions.add(tag)
-            return
-
-        super(Entity, self).__delattr__(attr)
 
 
 class StaleEntityInstanceError(Exception):
---
+++ pyhaystack/client/entity/tags.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Entity tag interface.  This file implements the interfaces that will be used
+to access and store tags of an entity.
+"""
+
+import hszinc
+import collections
+
+class BaseEntityTags(object):
+    """
+    A base class for storing entity tags.
+    """
+
+    def __init__(self, session, entity_id):
+        """
+        Initialise a new high-level entity tag storage object.
+
+        :param session: The connection session object.
+        :param entity_id: The entity's fully qualified ID.
+        """
+
+        self._session = session
+        self._entity_id = entity_id
+        self._tags = {}
+
+    @property
+    def id(self):
+        """
+        Return the fully qualified ID of this entity.
+        """
+        return hszinc.Ref(self._entity_id)
+
+    def __iter__(self):
+        """
+        Iterate over the tags present.
+        """
+        return iter(self._tags)
+
+    def __len__(self):
+        """
+        Return the number of tags present.
+        """
+        return len(self._tags)
+
+    def __getitem__(self, tag):
+        """
+        Return the value of a tag.
+        """
+        return self._tags[tag]
+
+    def _update_tags(self, tags):
+        """
+        Update the value of given tags.
+        """
+        stale = set(self._tags.keys())
+        for tag, value in tags.items():
+            if value is hszinc.REMOVE:
+                continue
+            else:
+                self._tags[tag] = value
+                stale.discard(tag)
+
+        for tag in stale:
+            self._tags.pop(tag, None)
+
+
+class BaseMutableEntityTags(BaseEntityTags):
+    """
+    A base class for entity tags that supports modifications to the tag set.
+    """
+    def __init__(self, session, entity_id):
+        super(BaseMutableEntityTags, self).__init__(session, entity_id)
+        self._tag_updates = {}
+        self._tag_deletions = set()
+
+    @property
+    def is_dirty(self):
+        """
+        Returns true if there are modifications pending submission.
+        """
+        return bool(self._tag_updates) or bool(self._tag_deletions)
+
+    def commit(self, callback=None):
+        """
+        Commit any to-be-sent updates for this entity.
+        """
+        raise NotImplementedError('TODO: implement CRUD ops')
+
+    def delete(self, callback=None):
+        """
+        Delete the entity.
+        """
+        raise NotImplementedError('TODO: implement CRUD ops')
+
+    def revert(self, tags=None):
+        """
+        Revert the named attribute changes, or all changes.
+        """
+        if tags is None:
+            self._tag_updates = {}
+            self._tag_deletions = set()
+        else:
+            for tag in tags:
+                self._tag_updates.pop(tag, None)
+                self._tag_deletions.discard(tag)
+
+    def __iter__(self):
+        """
+        Iterate over the tags present.
+        """
+        return iter(self._tag_names)
+
+    def __len__(self):
+        """
+        Return the number of tags present.
+        """
+        return len(self._tag_names)
+
+    def __getitem__(self, tag):
+        """
+        Return the value of a tag.
+        """
+        if tag in self._tag_deletions:
+            raise KeyError(tag)
+
+        try:
+            return self._tag_updates[tag]
+        except KeyError:
+            return self._tags[tag]
+
+    def __setitem__(self, tag, value):
+        """
+        Set the value of a tag.
+        """
+        if value is hszinc.REMOVE:
+            del self[tag]
+            return
+
+        self._tag_updates[tag] = value
+        self._tag_deletions.discard(tag)
+
+    def __delitem__(self, tag):
+        """
+        Remove a tag.
+        """
+        self._tag_deletions.add(tag)
+        self._tag_updates.pop(tag, None)
+
+    @property
+    def _tag_names(self):
+        """
+        Return a set of tag names present.
+        """
+        return (set(self._tags.keys()) | set(self._tag_updates.keys())) \
+                - self._tag_deletions
+
+
+class ReadOnlyEntityTags(BaseEntityTags, collections.Mapping):
+    pass
+
+
+class MutableEntityTags(BaseMutableEntityTags, collections.MutableMapping):
+    pass
