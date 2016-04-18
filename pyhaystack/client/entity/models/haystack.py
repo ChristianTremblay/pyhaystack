9b38aa9d316eebcad198a02f755f091738aea94c
Stuart Longland <stuartl@vrt.com.au>
2016-04-18 15:16:56 +1000

    client.entity.models.haystack: Project Haystack Tagging Model
    This should have gone in earlier actually, this implements a partial
    tagging model for Project Haystack.

---
+++ pyhaystack/client/entity/models/haystack.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tagging Model Interface for Project Haystack
+"""
+
+import hszinc
+from ..model import TaggingModel
+from ..mixins import tz, site
+
+class HaystackTaggingModel(TaggingModel):
+    """
+    An implementation of the Project Haystack tagging model.
+    """
+
+    def _identify_types(self, tags):
+        """
+        Inspect the tags on the to-be-created object and return a tuple
+        consisting of:
+        - the suggested class name
+        - a list of class instances that represent the add-on types for
+          that object.
+        """
+        types = []
+        names = []
+        if 'tz' in tags:
+            # We have a timezone
+            types.append(tz.TzMixin)
+            names.append('Tz')
+
+        if 'site' in tags:
+            # This is a site
+            types.append(site.SiteMixin)
+            names.append('Site')
+
+        return ('%sEntity' % ''.join(sorted(names)), types)
