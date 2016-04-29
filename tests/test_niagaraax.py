574d5dd5c7bdb6fb559fd967047a3310fea7f3c0
Christian Tremblay <christian.tremblay@servisys.com>
2016-04-28 22:14:02 -0400

    First really basic test so we start with something
    Signed-off-by: Christian Tremblay <christian.tremblay@servisys.com>

---
+++ tests/test_niagaraax.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+First test... just import something...
+"""
+import pytest
+
+from pyhaystack.client.niagara import NiagaraHaystackSession
+
+@pytest.fixture(scope='module')
+def session(request):
+    session = NiagaraHaystackSession(uri='http://www.myserver.com', 
+                                     username='user_name', 
+                                     password='M87h$&')
+    
+    def terminate():
+        session = None
+        print("It's over")
+    request.addfinalizer(terminate)
+    return session
+    
+def test_session_username(session):
+    assert session._username == 'user_name'
+    
+def test_session_password(session):
+    assert session._password == 'M87h$&'
