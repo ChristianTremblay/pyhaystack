357284b53dcbed18f96d2feddb0878abc0ae683b
Stuart Longland <stuartl@vrt.com.au>
2017-03-13 09:29:51 +1000

    FIR-214: client.widesky: Add hook to detect 401 error.
    If we have a 401 error message thrown at us, assume we've been logged
    out.  This could be delivered by way of a response or an exception.
    
    We should not raise an exception ourselves, but instead just invalidate
    the session.

--- pyhaystack/client/widesky.py
+++ pyhaystack/client/widesky.py
@@ -9,6 +9,8 @@ from .session import HaystackSession
 from .ops.vendor.widesky import WideskyAuthenticateOperation, \
         CreateEntityOperation, WideSkyHasFeaturesOperation
 from .mixins.vendor.widesky import crud, multihis
+from ..util.asyncexc import AsynchronousException
+from .http.exceptions import HTTPStatusError
 
 def _decode_str(s, enc='utf-8'):
     """
@@ -66,6 +68,24 @@ class WideskyHaystackSession(crud.CRUDOpsMixin,
         return (self._auth_result.get('expires_in') or 0.0) > (1000.0 * time())
 
     # Private methods/properties
+
+    def _on_http_response(self, response):
+        # If there's a '401' error, then we've lost the token.
+        if isinstance(response, AsynchronousException):
+            try:
+                response.reraise()
+            except HTTPStatusError as e:
+                status_code = e.status
+            except:
+                # Anything else, no-op … let the state machine handle it!
+                return
+        else:
+            status_code = response.status_code
+
+        if (status_code == 401) and (self._auth_result is not None):
+            self._log.warning('Authentication lost due to HTTP error 401.')
+            self._auth_result = None
+            self._client.headers = {}
 
     def _on_authenticate_done(self, operation, **kwargs):
         """
