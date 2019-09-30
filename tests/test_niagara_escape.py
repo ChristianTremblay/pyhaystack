981fa749f1b75d99af34979f116ba3e0e4e96a7f
Christian Tremblay, ing <christian.tremblay@servisys.com>
2019-09-29 22:04:01 -0400

    Will skip unescaping test for python 2... not supported

--- tests/test_niagara_escape.py
+++ tests/test_niagara_escape.py
@@ -3,10 +3,11 @@
 First test... just import something...
 """
 import pytest
+import sys
 
 from pyhaystack.client.niagara import Niagara4HaystackSession
 
-
+@pytest.mark.skipif(sys.version_info < (3, 4), reason="requires python3 or higher")
 def test_conversion_of_str():
     unescape = Niagara4HaystackSession.unescape
     dct = {
